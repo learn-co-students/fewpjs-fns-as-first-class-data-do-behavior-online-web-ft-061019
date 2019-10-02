8b066f6c82cc2135a435d33630f8f79e3fd60360
Enoch2k2 <enoch2k2@hotmail.com>
2019-10-02 17:52:10 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+const greet = (string) => {
+  let [hours, minutes] = string.split(":");
+  hours = parseInt(hours, 10);
+  minutes = parseInt(minutes, 10);
+
+  if(hours < 12) {
+    return "Good Morning";
+  } else if (hours >= 12 && hours <= 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+const displayMessage = (string) => {
+  document.querySelector("#greeting").innerText = string;
+}
